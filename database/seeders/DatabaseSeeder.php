cb1d2abea8d9a79a443f148bfd87c01381e75dd6
iostream-code <aryakmifjannah2603@gmail.com>
2024-01-22 10:49:19 +0700

    modify some files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,
             VillageSeeder::class,
             UserSeeder::class,
             ProfileSeeder::class,
